461be103dff091ce7ee0c2e3114dfef36255908b
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-23 11:08:42 +0700

    add model Product

---
+++ src/main/java/com/bayu/product/model/Product.java
@@ -0,0 +1,29 @@
+package com.bayu.product.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String brand;
+
+    private String madeIn;
+
+    private float price;
+}
