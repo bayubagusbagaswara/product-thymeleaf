cc925b3f51bb52452ff5d3f883041223da88ea5f
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-23 09:55:37 +0700

    Initial commit

---
+++ src/main/java/com/bayu/product/ProductThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductThymeleafApplication.class, args);
+	}
+
+}
