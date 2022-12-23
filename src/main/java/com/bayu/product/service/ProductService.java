abf533f390cc281d9859cd159f20222626896f95
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-23 11:09:13 +0700

    add service ProductService

---
+++ src/main/java/com/bayu/product/service/ProductService.java
@@ -0,0 +1,36 @@
+package com.bayu.product.service;
+
+import com.bayu.product.model.Product;
+import com.bayu.product.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> listAll() {
+        return productRepository.findAll();
+    }
+
+    public void save(Product product) {
+        productRepository.save(product);
+    }
+
+    public Product get(long id) {
+        return productRepository.findById(id).get();
+    }
+
+    public void delete(long id) {
+        productRepository.deleteById(id);
+    }
+
+}
